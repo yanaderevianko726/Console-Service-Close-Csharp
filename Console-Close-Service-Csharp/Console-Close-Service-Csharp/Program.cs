58a4ffc2b09ffd63f64d8b22b15a758a120a9b93
Yana DEREVIANKO <79232031+yanaderevianko726@users.noreply.github.com>
2023-05-29 16:13:38 +0700

    Initialize

---
+++ Console-Close-Service-Csharp/Console-Close-Service-Csharp/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Runtime.InteropServices;
+
+namespace Console_Close_Service_Csharp
+{
+    class Program
+    {
+        [DllImport("kernel32.dll")]
+        static extern IntPtr GetConsoleWindow();
+
+        [DllImport("user32.dll")]
+        static extern bool ShowWindow(IntPtr hWnd, int nCmdShow);
+
+        const int SW_HIDE = 0;
+        const int SW_SHOW = 5;
+
+        static void Main(string[] args)
+        {
+            var handle = GetConsoleWindow();
+            // Hide
+            ShowWindow(handle, SW_SHOW);
+        }
+    }
+}
